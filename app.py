9c42390e439bc44236e8cf27788a6cb68724fe4a
Alexis Edwards <101596613+ASP22SCM65E@users.noreply.github.com>
2022-04-21 19:57:15 -0500

    Added forecasting code

--- app.py
+++ app.py
@@ -79,6 +79,24 @@ def github():
     # Convert the data obtained from GitHub API to JSON format
     repository = repository.json()
 
+    # -------- DATA FOR FORECASTING --------
+    # "pulls_url"
+    pulls = repository_url + "/pulls?per_page=100"
+
+    # "commits_url"
+    commits = repository_url + "/commits?per_page=100"
+
+    # "branches_url"
+    branches = repository_url + "/branches?per_page=100"
+
+    # "collaborators_url"
+    collaborators = repository_url + "/collaborators?per_page=100"
+
+    # "releases_url"
+    releases = repository_url + "/releases?per_page=100"
+
+    # --------------------------------------
+
     today = date.today()
 
     issues_reponse = []
@@ -99,6 +117,31 @@ def github():
         query_url = GITHUB_URL + "search/issues?q=" + search_query + "&" + per_page
         # requsets.get will fetch requested query_url from the GitHub API
         search_issues = requests.get(query_url, headers=headers, params=params)
+
+        # -------- DATA FOR FORECASTING --------
+
+        pull_data = requests.get(pulls)
+        temp_text = pull_data.text
+        pull_data = json.loads(temp_text)
+
+        commit_data = requests.get(commits)
+        temp_text = commit_data.text
+        commit_data = json.loads(temp_text)
+
+        branch_data = requests.get(branches)
+        temp_text = branch_data.text
+        branch_data = json.loads(temp_text)
+
+        collaborator_data = requests.get(collaborators)
+        temp_text = collaborator_data.text
+        collaborator_data = json.loads(temp_text)
+
+        release_data = requests.get(releases)
+        temp_text = release_data.text
+        release_data = json.loads(temp_text)
+
+        # --------------------------------------
+
         # Convert the data obtained from GitHub API to JSON format
         search_issues = search_issues.json()
         issues_items = []
@@ -115,6 +158,7 @@ def github():
         for issue in issues_items:
             label_name = []
             data = {}
+
             current_issue = issue
             # Get issue number
             data['issue_number'] = current_issue["number"]
@@ -135,9 +179,41 @@ def github():
             data['Author'] = current_issue["user"]["login"]
             issues_reponse.append(data)
 
+        # -------- DATA FOR FORECASTING --------
+
+        pull_reponse = []
+        commit_reponse = []
+        branch_reponse = []
+        collaborator_reponse = []
+        release_reponse = []
+
+        data_sets = [pull_data, commit_data, branch_data, collaborator_data, release_data]
+        data_responses = [pull_reponse, commit_reponse, branch_reponse, collaborator_reponse, release_reponse]
+        for i in range(len(data_sets)):
+            data = {}
+            x = data_sets[i]
+            if x is None:
+                continue
+            else:
+                for y in x:
+                    data['pull_created_at'] = y['created_at'][0:10]
+                data_responses[i].append(data)
+
+        # --------------------------------------
+
         today = last_month
 
     df = pd.DataFrame(issues_reponse)
+
+    # -------- DATA FOR FORECASTING --------
+
+    df_pulls = pd.DataFrame(pull_reponse)
+    df_commits = pd.DataFrame(commit_reponse)
+    df_branches = pd.DataFrame(branch_reponse)
+    df_collaborators = pd.DataFrame(collaborator_reponse)
+    df_releases = pd.DataFrame(release_reponse)
+
+    # --------------------------------------
 
     # Daily Created Issues
     df_created_at = df.groupby(['created_at'], as_index=False).count()
@@ -179,6 +255,82 @@ def github():
         array = [str(key), month_issue_closed_dict[key]]
         closed_at_issues.append(array)
 
+
+
+    # -------- DATA FOR FORECASTING --------
+
+    # Pulls
+    df_pulls = df_pulls.sort_values(ascending=True)
+    pull_month = pd.to_datetime(
+        pd.Series(df_pulls), format='%Y/%m/%d')
+    pull_month.index = pull_month.dt.to_period('m')
+    pull_month = pull_month.groupby(level=0).size()
+    pull_month = pull_month.reindex(pd.period_range(
+        pull_month.index.min(), pull_month.index.max(), freq='m'), fill_value=0)
+    pull_month = pull_month.to_dict()
+    p_data = []
+    for key in pull_month.keys():
+        array = [str(key), pull_month[key]]
+        p_data.append(array)
+
+    # Commits
+    df_commits = df_commits.sort_values(ascending=True)
+    commit_month = pd.to_datetime(
+        pd.Series(df_commits), format='%Y/%m/%d')
+    commit_month.index = commit_month.dt.to_period('m')
+    commit_month = commit_month.groupby(level=0).size()
+    commit_month = commit_month.reindex(pd.period_range(
+        commit_month.index.min(), commit_month.index.max(), freq='m'), fill_value=0)
+    commit_month = commit_month.to_dict()
+    comm_data = []
+    for key in commit_month.keys():
+        array = [str(key), commit_month[key]]
+        comm_data.append(array)
+
+    # Branches
+    df_branches = df_branches.sort_values(ascending=True)
+    branch_month = pd.to_datetime(
+        pd.Series(df_branches), format='%Y/%m/%d')
+    branch_month.index = branch_month.dt.to_period('m')
+    branch_month = branch_month.groupby(level=0).size()
+    branch_month = branch_month.reindex(pd.period_range(
+        branch_month.index.min(), branch_month.index.max(), freq='m'), fill_value=0)
+    branch_month = branch_month.to_dict()
+    b_data = []
+    for key in branch_month.keys():
+        array = [str(key), branch_month[key]]
+        b_data.append(array)
+
+    # Collaborators
+    df_collaborators = df_collaborators.sort_values(ascending=True)
+    collaborator_month = pd.to_datetime(
+        pd.Series(df_collaborators), format='%Y/%m/%d')
+    collaborator_month.index = collaborator_month.dt.to_period('m')
+    collaborator_month = collaborator_month.groupby(level=0).size()
+    collaborator_month = collaborator_month.reindex(pd.period_range(
+        collaborator_month.index.min(), collaborator_month.index.max(), freq='m'), fill_value=0)
+    collaborator_month = collaborator_month.to_dict()
+    coll_data = []
+    for key in collaborator_month.keys():
+        array = [str(key), collaborator_month[key]]
+        coll_data.append(array)
+
+    # Releases
+    df_releases = df_releases.sort_values(ascending=True)
+    release_month = pd.to_datetime(
+        pd.Series(df_releases), format='%Y/%m/%d')
+    release_month.index = release_month.dt.to_period('m')
+    release_month = release_month.groupby(level=0).size()
+    release_month = release_month.reindex(pd.period_range(
+        release_month.index.min(), release_month.index.max(), freq='m'), fill_value=0)
+    release_month = release_month.to_dict()
+    r_data = []
+    for key in release_month.keys():
+        array = [str(key), release_month[key]]
+        r_data.append(array)
+
+    # --------------------------------------
+
     '''
         1. Hit LSTM Microservice by passing issues_response as body
         2. LSTM Microservice will give a list of string containing image paths hosted on google cloud storage
@@ -195,6 +347,36 @@ def github():
         "type": "closed_at",
         "repo": repo_name.split("/")[1]
     }
+
+    # -------- DATA FOR FORECASTING --------
+
+    pulls_body = {
+        "issues": pull_reponse,
+        "type": "pulls",
+        "repo": repo_name.split("/")[1]
+    }
+    commits_body = {
+        "issues": commit_reponse,
+        "type": "commits",
+        "repo": repo_name.split("/")[1]
+    }
+    branches_body = {
+        "issues": branch_reponse,
+        "type": "branches",
+        "repo": repo_name.split("/")[1]
+    }
+    collaborators_body = {
+        "issues": collaborator_reponse,
+        "type": "collaborators",
+        "repo": repo_name.split("/")[1]
+    }
+    releases_body = {
+        "issues": release_reponse,
+        "type": "releases",
+        "repo": repo_name.split("/")[1]
+    }
+
+    # --------------------------------------
 
     # Update your Google cloud deployed LSTM app URL (NOTE: DO NOT REMOVE "/")
     LSTM_API_URL = "https://lstm-7fxxr4rxjq-uc.a.run.app/" + "api/forecast"
@@ -217,6 +399,28 @@ def github():
                                        json=closed_at_body,
                                        headers={'content-type': 'application/json'})
     
+
+    # -------- DATA FOR FORECASTING --------
+
+    response_p = requests.post(LSTM_API_URL,
+                                        json=pulls_body,
+                                        headers={'content-type': 'application/json'})
+    response_comm = requests.post(LSTM_API_URL,
+                                        json=commits_body,
+                                        headers={'content-type': 'application/json'})
+    response_b = requests.post(LSTM_API_URL,
+                                        json=branches_body,
+                                        headers={'content-type': 'application/json'})
+    response_coll = requests.post(LSTM_API_URL,
+                                        json=collaborators_body,
+                                        headers={'content-type': 'application/json'})
+    response_r = requests.post(LSTM_API_URL,
+                                        json=releases_body,
+                                        headers={'content-type': 'application/json'})
+
+    # --------------------------------------
+
+
     '''
     Create the final response that consists of:
         1. GitHub repository data obtained from GitHub API
@@ -232,6 +436,21 @@ def github():
         },
         "closedAtImageUrls": {
             **closed_at_response.json(),
+        },
+        "pullsImageUrls": {
+            **response_p.json(),
+        },
+        "commitsImageUrls": {
+            **response_comm.json(),
+        },
+        "branchesImageUrls": {
+            **response_b.json(),
+        },
+        "collaboratorsImageUrls": {
+            **response_coll.json(),
+        },
+        "releasesImageUrls": {
+            **response_r.json(),
         },
     }
     # Return the response back to client (React app)
